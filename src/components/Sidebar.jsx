22f4b10554f277ba29f57828f69b10529e3760f8
Mobina Abforoosh <mobinaabf@gmail.com>
2023-09-09 10:36:56 +0330

    add menuItem array and add objects in array with path,name and icon name for each page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,48 @@
 import React from "react";
-
+import {
+FaTh,
+FaUser,
+FaRegChartBar,
+FaCommentAlt,
+FaShoppingBag,
+FaThList
+} from "react-icons/fa";
 export default function sidebar() {
+  const menuItem[
+    {
+      path:"/",
+      name:"Dashboard",
+      icon:<FaTh/>
+    },
+    {
+      path:"/about",
+      name:"About",
+      icon:<FaUser/>
+    },
+    {
+      path:"/analystic",
+      name:"Analystic",
+      icon:<FaRegChartBar/>
+    },
+    {
+      path:"/comment",
+      name:"Comment",
+      icon:<FaCommentAlt/>
+    },
+    {
+      path:"/product",
+      name:"Product",
+      icon:<FaShoppingBag/>
+    },
+    {
+      path:"/productlist",
+      name:"ProductList",
+      icon:<FaThList/>
+    }
+  ];
+
   return (
-    <div>
+    <div >
       <h1>this is sidebar</h1>
     </div>
   );
