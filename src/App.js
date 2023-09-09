5b4e027b66c733e0bad72223ebc99bbe16dc50e0
Mobina Abforoosh <mobinaabf@gmail.com>
2023-09-09 10:17:35 +0330

    import Route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from "react";
-import Sidebar from "./components/Sidebar";
+import { Route } from 'react-router-dom';
+// import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard.jsx";
-import About from './pages/About.jsx'
-import Comment from './pages/Comment.jsx'
-import Analystic from './pages/Analystic.jsx'
-import Product from './pages/Product.jsx'
-import ProductList from './pages/ProductList.jsx'
+import About from "./pages/About.jsx";
+import Comment from "./pages/Comment.jsx";
+import Analystic from "./pages/Analystic.jsx";
+import Product from "./pages/Product.jsx";
+import ProductList from "./pages/ProductList.jsx";
 import { BrowserRouter, Routes } from "react-router-dom";
 
 export default function App() {
-  return <BrowserRouter>
-  <Routes>
-    <Route path="/"element={<Dashboard/}/>
-    <Route path="/dashboard"element={<Dashboard/}/>
-    <Route path="/about"element={<About/}/>
-    <Route path="/comment"element={<Comment/}/>
-    <Route path="/analystic"element={<Analystic/}/>
-    <Route path="/product"element={<Product/}/>
-    <Route path="/productlist"element={<ProductList/}/>
-
-
-  </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/comment" element={<Comment />} />
+        <Route path="/analystic" element={<Analystic />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/productlist" element={<ProductList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
