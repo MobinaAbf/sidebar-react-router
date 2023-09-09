d90d2bc6d58cc3105835b4a2505472726eaec7d8
Mobina Abforoosh <mobinaabf@gmail.com>
2023-09-09 07:57:57 +0330

    create About , Analystic ,Comment ,Product and ProductList (.jsx) in pages folder

---
+++ src/pages/Analystic.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Analystic() {
+  return (
+    <div>
+      <h1>Analystic page</h1>
+    </div>
+  );
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Dashboard() {
   return (
-    <div><h1>dashboard page</h1></div>
-  )
+    <div>
+      <h1>dashboard page</h1>
+    </div>
+  );
 }
